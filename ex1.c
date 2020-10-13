2aff245d1a65725e3fd39c50a1f4b71bbcc8aa20
Duck2 <taikhoanemail109@gmail.com>
2020-10-13 21:08:46 +0700

    ex1

---
+++ ex1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	printf(" Hours of Precipitation: 3hrs  \t\t Hours of Precipitation: 1hrs \n");
+	printf(" Hours of Rains: 3hrs  \t\t\t Hours of Rains: 1hrs \n\n\n ");
+	
+	printf("TEMPERATURE HISTORY SEP 19\n\n");
+	printf(" \t Today \t\t Normal \t Record \t 9/19/2017 \n" );	
+	printf(" High \t 31C \t\t 31C \t\t N/A \t\t 35C \n" );	
+	printf(" Low \t 25C \t\t 24C \t\t N/A \t\t 28C \n\n" );
+	
+	printf(" SUNRISE/SUNSET \t\t MOONRISE/MOONSET \n");
+	printf(" Sunrise: 5:44 AM \t\t Moonrise: 2:11 PM \n");
+	printf(" Sunset: 5:56 PM \t\t Moonset: 1:26 AM \n\n");
+	printf(" Duration: 12:12 hr \t\t Duration: 11:15 hr \n");	
+
+		
+	return 0;
+}
