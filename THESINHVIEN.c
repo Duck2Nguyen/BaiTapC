c2db77c9f7c99ee90d1eafe69b3943d6b9ecf162
Duck2 <taikhoanemail109@gmail.com>
2020-10-13 20:49:44 +0700

    ex2

---
+++ THESINHVIEN.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	printf(" Attacking Statistics \n" );	
+	printf(" Goals \t\t\t\t 23 \n" );
+	printf(" Shots/Goals Ratio \t\t 0.17  \n" );
+	printf(" Shots on Target Ratio \t\t 0.59\n\n" );
+		
+	printf(" Defensive Statistics \n" );	
+	printf(" Goals Conceded/Game \t\t 1.19 \n" );
+	printf(" Shots/Goals Ratio \t\t 0.17  \n" );
+	printf(" Shots on Target Ratio \t\t 0.59\n" );
+		
+	printf("\n Attacking Statistics \n" );	
+	printf(" Goals \t\t\t\t 23 \n" );
+	printf(" Shots/Goals Ratio \t\t 0.22  \n" );
+	printf(" Shots on Target Ratio \t\t 0.43\n" );
+	
+	printf("\n Attacking Statistics \n" );	
+	printf(" Red Cards \t\t\t 0 \n" );
+	printf(" Yelow Cards \t\t\t 5 \n" );
+	printf(" Total Cards \t\t\t 6 \n" );
+	printf(" Fouls \t\t\t\t 23 \n" );
+	
+	return 0;
+}
